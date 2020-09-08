a15874b00561dcd4808d164b50c3cc32a78eaf42
Amanda Nelson <anelson99@ku.edu>
2020-09-07 19:49:26 -0500

    Initial checkForShip function defined

--- battleship.js
+++ battleship.js
@@ -13,5 +13,34 @@ var numShips = 0;
 */
 
 
+var row = '';
+var col = ''
+function checkForShip(row, col)
+{
+    if(player == 1) {
+       if(board2[row-1][col-1] == '*') {
+           board2[row-1][col-1] = 'M';
+           document.querySelector("#result").innerText = " MISS "
+       }
+       else {
+           board2[row-1][col-1] = 'H'
+           document.querySelector("#result").innerText = " HIT "
+       }
+       player = 2;
+    }
+    else {
+        if(board2[row-1][col-1] == '*') {
+            board2[row-1][col-1] = 'M';
+            document.querySelector("#result").innerText = " MISS "
+        }
+        else {
+            board2[row-1][col-1] = 'H'
+            document.querySelector("#result").innerText = " HIT "
+        }
+        player = 1;
+    }
+}
+
+
 
 
