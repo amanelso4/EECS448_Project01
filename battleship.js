643f3ef4fe9026854e5ca911de2db7e29be24c06
Reece Mathews <mathewsreece@gmail.com>
2020-09-10 17:33:26 -0500

    Create working place ships stage

--- battleship.js
+++ battleship.js
@@ -4,32 +4,54 @@ var board1 = [];
 var board2 = [];
 var numShips = 0;
 
+var horizontal = true;
+var placing = true;
+var placingNum = 1;
+
 /* * = empty
     M = Miss
     H = Hit
     @ = Ship
 */
 
+
+function numShipFunction(num){
+    document.getElementById('ships').innerHTML = 'Place your ' + placingNum + '-length ship.';
+    numShips = num;
+
+    for(var i = 1; i < 6; i++){
+        document.getElementById(i + "Ship").remove();
+    }
+};
+
+function toggleDirection() {
+    horizontal = !horizontal;
+
+    let place_dir = "";
+    if (horizontal) {
+        place_dir = "Horizontally";
+    } else {
+        place_dir = "Vertically";
+    }
+    document.getElementById('toggleDir').innerHTML = 'Placing ' + place_dir;
+}
+
+
 //length: length of the ship
 //horizontal: true if the boat is being placed horizontally
 function placeShip(row,col,board,length,horizontal){
   if(checkPlacement(row,col,board,length,horizontal)){
     for(i = 0; i < length; i++){
-      if(board == 1){
         if(horizontal){
-          board1[row][col+i]=length;
+            board[row][col+i]=length;
         }else{
-          board1[row+i][col]=length;
+            board[row+i][col]=length;
         }
-      }
-      else{
-        if(horizontal){
-          board2[row][col+i]=length;
-        }else{
-          board2[row+i][col]=length;
-        }
-      }
     }
+    drawPlayerBoard(board);
+    return true;
+  } else {
+      return false;
   }
 }
 function checkPlacement(row,col,board,length,horizontal){
@@ -39,28 +61,20 @@ function checkPlacement(row,col,board,length,horizontal){
       valid = false;
     }
     else{
-      for(i = 0; i < length; i++){
-        if(board == 1 && board1[row+i][col]!="*"){
-          valid = false;
-          break;
-        }
-        if(board == 2 && board2[row+i][col]!="*"){
+      for(let i = 0; i < length; i++){
+        if(board[row][col+i]!="*"){
           valid = false;
           break;
         }
       }
     }
   }else{
-    if(9 > (row+length)){
+    if(9 < (row+length)){
       valid = false;
     }
     else{
-      for(i = 0; i < length; i++){
-        if(board == 1 && board1[row+i][col]!="*"){
-          valid = false;
-          break;
-        }
-        if(board == 2 && board2[row+i][col]!="*"){
+      for(let i = 0; i < length; i++){
+        if(board[row+i][col]!="*"){
           valid = false;
           break;
         }
@@ -82,14 +96,55 @@ function createBoards(){
   }
 }
 
-function clickCheck(row, col)
+
+function setup() {
+    createBoards();
+}
+
+
+function clickCheck(col, row)
 {
-    checkForShip(row,col);
-   // checkForWinner(row,ship);
+    col--;  // Account for 1-indexing in HTML code
+    row--;
+    console.log(row, col);
+    if (placing) {
+        if (numShips == 0) {
+            return;
+        }
+        if (placingNum <= numShips) {
+            if (placeShip(row, col, getBoard(), placingNum, horizontal)) {
+                placingNum++;
+            }
+        }
+        if (placingNum > numShips) {
+            if (player == 1) {
+                placingNum = 1;
+                player = 2;
+            } else {
+                placing = false;
+            }
+        }
+        if (placing) {
+            document.getElementById('ships').innerHTML = 'Place your ' + placingNum + '-length ship.';
+        } else {
+            document.getElementById('ships').innerHTML = 'CHOOSE WHERE TO SHOOT.';
+        }
+    } else {
+        checkForShip(row,col);
+    }
+}
+
+function getBoard() {
+    if(player == 1) {
+        return board1;
+    }
+    else {
+        return board2;
+    }
 }
 
 function switchPlayer() {
-    if(player = 1) {
+    if(player == 1) {
         player = 2;
         drawGuessBoard(board1);
         drawPlayerBoard(board2);
@@ -107,21 +162,30 @@ function drawGuessBoard(newBoard) {
         for(var j = 0; j<9; j++) {
             if(newBoard[i][j] != '@')
             {
-                document.querySelector("cell"+(i+1)+(j+1)).innerText = newBoard [i][j];
+                document.querySelector("cell"+(j+1)+(i+1)).innerText = newBoard[i][j];
             }
             else {
-                document.querySelector("cell"+(i+1)+(j+1)).innerText = '*';            }
+                document.querySelector("cell"+(j+1)+(i+1)).innerText = '*';            }
         }
     }
 }
 
+function colorCell(row, col) {
+    row++; // Account for HTML 1-indexing
+    col++;
+    document.getElementById('cell'+col+row).classList.remove('empty');
+    document.getElementById('cell'+col+row).classList.add('red');
+}
+
 function drawPlayerBoard(newBoard) {
     num = 1;
-    let coord = document.getElementById("playerBoard"+1+1);
     for(var i = 0; i<9; i++){
         for(var j = 0; j<9; j++) {
-            coord = newBoard[i][j]
-            coord = document.getElementById("guessBoard" + (i+1) + (j+1));
+            coord = newBoard[i][j];
+            if (coord != "*") {
+                colorCell(i, j);
+            }
+            coord = document.getElementById("guessBoard" + (j+1) + (i+1));
         }
     }
 }
@@ -204,3 +268,10 @@ function checkForWinner()
     }
   return won;
 }
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    setup();
+    // document.addEventListener("resize", draw_screen);
+    // window.onresize = draw_screen;
+});
