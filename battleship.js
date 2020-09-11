62c26545d978e651dbf934bc7c4351fcde97e7db
Reece Mathews <mathewsreece@gmail.com>
2020-09-11 14:48:12 -0500

    Add documentation to all functions

--- battleship.js
+++ battleship.js
@@ -70,6 +70,15 @@ function placeShip(row,col,board,length,horizontal){
   }
 }
 
+/**
+ * Check if a ship can be placed at the given point on the board.
+ * 
+ * @param {number} row The row of the board in which to place the ship
+ * @param {number} col The column of the board in which to place the ship
+ * @param {Array} board The board for the current player
+ * @param {number} length The length of the ship to be placed
+ * @param {bool} horizontal Flag to place ship horizontally or otherwise vertically
+ */
 function checkPlacement(row,col,board,length,horizontal){
   let valid = true;
   if(horizontal){
@@ -100,6 +109,9 @@ function checkPlacement(row,col,board,length,horizontal){
   return valid;
 }
 
+/**
+ * Initialize the player boards to their default empty values (*).
+ */
 function createBoards(){
     console.log("the boards were created");
   for(i = 0; i < 9; i++){
@@ -112,12 +124,13 @@ function createBoards(){
   }
 }
 
-
-function setup() {
-    createBoards();
-}
-
-
+/**
+ * Handle clicks on the boards in the HTML.
+ * 
+ * @param {number} board_num The number of the board that was clicked.
+ * @param {number} col The column that was clicked.
+ * @param {number} row The row that was clicked.
+ */
 function clickCheck(board_num, col, row)
 {
     console.log(board_num, row, col);
@@ -152,6 +165,11 @@ function clickCheck(board_num, col, row)
     }
 }
 
+/**
+ * Get the current player board or by number.
+ * 
+ * @param {number} num The number of board to return. Returns current board if NaN.
+ */
 function getBoard(num=NaN) {
     if (isNaN(num)) {
         num = player;
@@ -164,6 +182,9 @@ function getBoard(num=NaN) {
     }
 }
 
+/**
+ * Check if the turn is over and then switch the current player.
+ */
 function switchPlayer() {
   if(waitForSwitch){
     if(player == 1) {
@@ -188,16 +209,30 @@ function switchPlayer() {
   }
   checkForWinner();
 }
+
+/**
+ * Set the boards to visible.
+ */
 function drawBoards(){
   document.getElementsByClassName('grid-container boardA')[0].style.visibility = 'visible';
   document.getElementsByClassName('grid-container boardB')[0].style.visibility = 'visible';
   document.getElementById('switch').style.visibility='visible';
 }
+
+/**
+ * Set the boards to invisible.
+ */
 function hideBoards(){
   document.getElementsByClassName('grid-container boardA')[0].style.visibility = 'hidden';
   document.getElementsByClassName('grid-container boardB')[0].style.visibility = 'hidden';
   document.getElementById('switch').style.visibility='hidden';
 }
+
+/**
+ * Draw the given player board to the guess board so that ships are hidden.
+ * 
+ * @param {Array} newBoard The player board to be drawn
+ */
 function drawGuessBoard(newBoard) {
     for(var i = 0; i<9; i++){
         for(var j = 0; j<9; j++) {
@@ -215,13 +250,12 @@ function drawGuessBoard(newBoard) {
     }
 }
 
-function colorCell(row, col) {
-    row++; // Account for HTML 1-indexing
-    col++;
-    document.getElementById('cell'+col+row).classList.remove('empty');
-    document.getElementById('cell'+col+row).classList.add('red');
-}
 
+/**
+ * Draw the given player board to the displayed player board so that ships are visible.
+ * 
+ * @param {Array} newBoard The player board to be drawn
+ */
 function drawPlayerBoard(newBoard) {
     for(var i = 0; i<9; i++){
         for(var j = 0; j<9; j++) {
@@ -239,42 +273,84 @@ function drawPlayerBoard(newBoard) {
     }
 }
 
+/**
+ * Color a cell as a ship on board one by row and column position.
+ * 
+ * @param {number} row The number of the row to color.
+ * @param {number} col The number of the column to color.
+ */
 function colorShip(row, col){
     document.getElementById('A'+col+row).classList.remove('empty');
     document.getElementById('A'+col+row).classList.remove('miss');
     document.getElementById('A'+col+row).classList.add('red');
 }
 
+/**
+ * Color a cell as a miss on board one by row and column position.
+ * 
+ * @param {number} row The number of the row to color.
+ * @param {number} col The number of the column to color.
+ */
 function colorMiss(row, col){
     document.getElementById('A'+col+row).classList.remove('empty');
     document.getElementById('A'+col+row).classList.remove('red');
     document.getElementById('A'+col+row).classList.add('miss');
 }
 
-
+/**
+ * Color a cell as a ship on board two by row and column position.
+ * 
+ * @param {number} row The number of the row to color.
+ * @param {number} col The number of the column to color.
+ */
 function colorShip2(row, col){
     document.getElementById('B'+col+row).classList.remove('empty');
     document.getElementById('B'+col+row).classList.remove('miss');
     document.getElementById('B'+col+row).classList.add('red');
 }
 
+/**
+ * Color a cell as a miss on board two by row and column position.
+ * 
+ * @param {number} row The number of the row to color.
+ * @param {number} col The number of the column to color.
+ */
 function colorMiss2(row, col){
     document.getElementById('B'+col+row).classList.remove('empty');
     document.getElementById('B'+col+row).classList.remove('red');
     document.getElementById('B'+col+row).classList.add('miss');
 }
 
+/**
+ * Color a cell as empty on board one by row and column position.
+ * 
+ * @param {number} row The number of the row to color.
+ * @param {number} col The number of the column to color.
+ */
 function colorBlue(row, col){
     document.getElementById('A'+col+row).classList.remove('red');
     document.getElementById('A'+col+row).classList.remove('miss');
     document.getElementById('A'+col+row).classList.add('empty');
 }
+
+/**
+ * Color a cell as empty on board two by row and column position.
+ * 
+ * @param {number} row The number of the row to color.
+ * @param {number} col The number of the column to color.
+ */
 function colorBlue2(row, col){
     document.getElementById('B'+col+row).classList.remove('red');
     document.getElementById('B'+col+row).classList.remove('miss');
     document.getElementById('B'+col+row).classList.add('empty');
 }
 
+/**
+ * Check for a ship on the enemy's board at the given location and color the cell accordingly.
+ * 
+ * @param {number} row The row to check.
+ * @param {number} col The column to check.
+ */
 function checkForShip(row, col)
 {
     if(player == 1) {
@@ -309,6 +385,9 @@ function checkForShip(row, col)
     }
 }
 
+/**
+ * Check if all ships have been hit.
+ */
 function checkForWinner()
 {
   var won = false;
@@ -362,9 +441,9 @@ function checkForWinner()
   return won;
 }
 
-
+/**
+ * Setup the game on page load.
+ */
 document.addEventListener("DOMContentLoaded", () => {
-    setup();
-    // document.addEventListener("resize", draw_screen);
-    // window.onresize = draw_screen;
+    createBoards();
 });
